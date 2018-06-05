0a23c4fbba5cc1f6f7d38c31214e2843ed538cbf
Ntokozo Zwane <ntkzwane@gmail.com>
2018-06-05 15:29:57 +0200

    Added requestbin environment

--- toolbox/requestbin.py
+++ toolbox/requestbin.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python
+
 from flask import Flask, request
-from flask.ext.cors import CORS
+from flask_cors import CORS
+
 app = Flask(__name__)
+
 CORS(app)
-@app.route('/', defaults={'path': ''}, methods=["GET", "POST", "OPTIONS"])
-@app.route('/<path:path>', methods=["GET", "POST", "OPTIONS"])
-def hello(path):
+
+@app.route('/', defaults={'path': ''}, methods=["GET", "PUT", "PATCH", "DELETE", "POST", "OPTIONS"])
+@app.route('/<path:path>', methods=["GET", "PUT", "PATCH", "DELETE", "POST", "OPTIONS"])
+def index(path):
     print ""
     print "path: %s\n" % path
     print "method: %s\n" % request.method
@@ -15,4 +20,30 @@ def hello(path):
     return "%s" % path
 
 if __name__ == "__main__":
+    print '################### Starting server ###################'
+    app.run(debug=True)
+
+#!/usr/bin/env python
+
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/', defaults={'path': ''}, methods=["GET", "PUT", "PATCH", "DELETE", "POST", "OPTIONS"])
+@app.route('/<path:path>', methods=["GET", "PUT", "PATCH", "DELETE", "POST", "OPTIONS"])
+def index(path):
+    print ""
+    print "path: %s\n" % path
+    print "method: %s\n" % request.method
+    print "headers:\n%s\n" % request.headers
+    print "body:\n %s\n" % request.data
+    print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"
+
+    return "%s" % path
+
+if __name__ == "__main__":
+    print '################### Starting server ###################'
     app.run(debug=True)
